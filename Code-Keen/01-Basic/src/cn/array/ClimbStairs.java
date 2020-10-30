5632e61d8782b3def8aac1638dc8f6687467ba02
keen <lizhangjie316@163.com>
2020-10-30 10:35:29 +0800

    修改代码

--- Code-Keen/01-Basic/src/cn/array/ClimbStairs.java
+++ Code-Keen/01-Basic/src/cn/array/ClimbStairs.java
@@ -10,18 +10,18 @@ public class ClimbStairs {
 }
 
 /**
-* 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * 注意：给定 n 是一个正整数。
-*/
+ */
 class Solution1 {
     protected int climbStairs(int n) {
         int f1 = 1;
         int f2 = 2;
         int f3 = 0;
-        if (1==n){
+        if (1 == n) {
             return f1;
-        }else if (2==n){
+        } else if (2 == n) {
             return f2;
         }
         for (int i = 2; i < n; i++) {
